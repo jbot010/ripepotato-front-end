fa8e6c4a436785c3f6eeecf3ff55d4003c99df05
Julian <julian.franco@icloud.com>
2023-06-04 19:22:32 -0400

    create profile display

--- src/pages/Profiles/Profiles.tsx
+++ src/pages/Profiles/Profiles.tsx
@@ -1,42 +1,52 @@
 // npm modules
-import { useState, useEffect } from 'react'
+// import { useState, useEffect } from 'react'
 
 // services
-import * as profileService from '../../services/profileService'
+// import * as profileService from '../../services/profileService'
 
 // css
 import styles from './Profiles.module.css'
 
+// components
+import ProfileCard from '../../components/ProfileCard/ProfileCard'
+
 // types
 import { Profile } from '../../types/models'
 
-const Profiles = (): JSX.Element => {
-  const [profiles, setProfiles] = useState<Profile[]>([])
+interface ProfilesProps {
+  profiles: Profile[];
+}
 
-  useEffect((): void => {
-    const fetchProfiles = async (): Promise<void> => {
-      try {
-        const profileData: Profile[] = await profileService.getAllProfiles()
-        setProfiles(profileData)
-      } catch (error) {
-        console.log(error)
-      }
-    }
-    fetchProfiles()
-  }, [])
+const Profiles = (props: ProfilesProps): JSX.Element => {
+  const { profiles } = props
+  // const [profiles, setProfiles] = useState<Profile[]>([])
+
+  // useEffect((): void => {
+  //   const fetchProfiles = async (): Promise<void> => {
+  //     try {
+  //       const profileData: Profile[] = await profileService.getAllProfiles()
+  //       setProfiles(profileData)
+  //     } catch (error) {
+  //       console.log(error)
+  //     }
+  //   }
+  //   fetchProfiles()
+  // }, [])
 
   if (!profiles.length) {
     return <main className={styles.container}><h1>Loading...</h1></main>
   }
 
   return (
-    <main className={styles.container}>
-      <h1>Hello. This is a list of all the profiles.</h1>
-      {profiles.map((profile: Profile) => (
-        <p key={profile.id}>{profile.name}</p>
-      ))}
+    <main className='list'>
+      {profiles.map((profile: Profile) =>
+        <ProfileCard
+          key={profile.id}
+          profile={profile}
+        />
+      )}
     </main>
   )
 }
- 
+
 export default Profiles
