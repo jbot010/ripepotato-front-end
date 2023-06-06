2306341457383b7e04186d0dadbe161e712410a3
Julian <julian.franco@icloud.com>
2023-06-06 14:49:06 -0400

    rename ratemanager to votemanager

---
+++ src/components/VoteManager/VoteManager.tsx
@@ -0,0 +1,15 @@
+// types
+import { Movie } from "../../types/models"
+
+interface VoteManagerProps {
+  movie: Movie;
+}
+
+const VoteManager = (): JSX.Element => {
+
+  return (
+
+  )
+}
+
+export default VoteManager
