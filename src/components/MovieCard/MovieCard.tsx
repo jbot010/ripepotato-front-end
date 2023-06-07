b79ae6d565f3092de4ba5afb98fc2dd10c867463
Julian <julian.franco@icloud.com>
2023-06-07 18:56:59 -0400

    built avg RP score fn

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -1,5 +1,5 @@
 // npm modules
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 // types
 import { Movie, User } from "../../types/models"
@@ -11,7 +11,7 @@ import VoteInput from "./VoteInput"
 
 interface MovieCardProps {
   movie: Movie
-  user: User | null
+  user: User
   onSubmit: (formData: movieFormData) => void
   onDelete: (movieId: number) => Promise<void>
 }
@@ -22,8 +22,12 @@ const MovieCard = (props: MovieCardProps): JSX.Element => {
   const [userScore, setUserScore] = useState<number>(0)
   const [avgScore, setAvgScore] = useState<number>(0)
 
+  useEffect((): void => {
+    getVotes()
+  },[movie])
+
   const checkIfIsCurrentUser = () => {
-    return user?.profile.id === movie.createdById
+    return user.profile.id === movie.createdById
   }
 
   const handleShowForm = () => {
@@ -40,24 +44,25 @@ const MovieCard = (props: MovieCardProps): JSX.Element => {
       onDelete(movie.id)
     }
   }
-  console.log("MOVIE CARD", { movie })
-
 
   const getVotes = () => {
-    let total = 0
+    if (movie.votesReceived && movie.votesReceived.length > 0) {
+      let total = 0
 
-    // map over movies.votesReceived
-    // forEach vote check voteId === user.profile.id
-    // if ids match, set user vote 
-    // update userScore
-    // forEach vote add value to total
-    
-    // math for total total / votesReceived.length 
-    // setAvgScore
+      movie.votesReceived.forEach((voteObj) => {
+        if (voteObj.voterId === user.profile.id) {
+          setUserScore(voteObj.value)
+        }
+        total = voteObj.value + total
+      })
 
+      const avg = Math.floor(total / movie.votesReceived.length)
+      setAvgScore(avg)
+    } else {
+      return
+    }
   }
-
-
+  
   return (
     <div
       style={{ border: "1px solid white" }}
@@ -76,12 +81,12 @@ const MovieCard = (props: MovieCardProps): JSX.Element => {
         <div>
           {/* <img src="./cinema.png" alt={`${movie.title}'s avatar`} /> */}
           <h1>{movie.title}</h1>
-          <h2>{movie.rtScore}</h2>
+          <h2>Rotten Tomato Score: {movie.rtScore}</h2>
         </div>
       )}
-      <h2> {avgScore} RIPEPOTATO SCORE HERE</h2>
-      <VoteInput defaultValue={userScore}/>
-    
+      <h2> Ripe Potato Score: {avgScore}</h2>
+      <VoteInput defaultValue={userScore} />
+
       {checkIfIsCurrentUser() ? (
         <button onClick={handleDeleteMovie}>DELETE MOVIE</button>
       ) : (
