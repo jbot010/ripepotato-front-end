1facf5f0f52b7385a50fdcd73e1fe3f5e9fa08e9
Julian <julian.franco@icloud.com>
2023-06-09 00:07:09 -0400

    fix errors

--- src/pages/Landing/Landing.tsx
+++ src/pages/Landing/Landing.tsx
@@ -1,7 +1,15 @@
 // css
 import styles from "./Landing.module.css"
+// types
+import { User } from "../../types/models"
 
-const Landing = (): JSX.Element => {
+interface LandingProps {
+  user: User | null
+}
+
+const Landing = (props: LandingProps): JSX.Element => {
+  const { user } = props
+
   return (
     <main className={styles.container}>
       <div className={styles.HERO}>
